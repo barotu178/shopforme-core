59b95a97acb5d94821c8544e36531b5735be6801
Obaro Aruotu <obaroaa289@gmail.com>
2026-02-19 15:48:11 -0500

    Add repositories for cart and cart items

--- src/main/java/com/shopforme/core/cart/CartItemRepository.java
+++ src/main/java/com/shopforme/core/cart/CartItemRepository.java
@@ -1,4 +1,10 @@
 package com.shopforme.core.cart;
 
-public class CartItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
 }
